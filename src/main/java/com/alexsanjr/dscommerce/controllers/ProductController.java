04fb9381954a35a059f5ab3113ae4dbbe9a6b652
Alex Sandro <alex.sandro.1199@gmail.com>
2023-12-23 08:30:11 -0300

    Cleaning the code

--- src/main/java/com/alexsanjr/dscommerce/controllers/ProductController.java
+++ src/main/java/com/alexsanjr/dscommerce/controllers/ProductController.java
@@ -1,15 +1,12 @@
 package com.alexsanjr.dscommerce.controllers;
 
 import com.alexsanjr.dscommerce.dto.ProductDTO;
-import com.alexsanjr.dscommerce.entities.Product;
 import com.alexsanjr.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
